e4e21fce30e58430c3544e56a7543feefbec66df
Fahad Ahmed <fahad.ahmed@me.com>
2023-04-17 10:26:49 +1000

    initial commit

---
+++ libs/components/src/lib/core/Button/Button.tsx
@@ -0,0 +1,8 @@
+import { Button } from '@carbon/react';
+import '../../../../node_modules/@carbon/styles/css/styles.min.css';
+
+export default function ButtonComponent() {
+  return(
+    <Button>Test Button</Button>
+  )
+}
